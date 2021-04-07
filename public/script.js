6773dbe41b07078b88191ee1a6017da72ccf7bcb
Sebi <sebilarion@gmail.com>
2021-04-07 23:02:37 +0300

    add modal window and javascript file

--- public/script.js
+++ public/script.js
@@ -1,20 +1,70 @@
-var modal = document.querySelector("#modal");
-// var modalOverlay = document.querySelector("#modal-overlay");
-var closeButton = document.querySelector("#close-modal");
-var openButton = document.querySelector("#open-modal");
-var uploadsButton = document.querySelector("#uploadsbtn");
-var uploads = document.querySelector("#uploads");
+const modal = document.querySelector("#modal");
+const closeButton = document.querySelector("#close-modal");
+const openButton1 = document.querySelector(".modal-open-1");
+const openButton2 = document.querySelector(".modal-open-2");
+const openButton3 = document.querySelector(".modal-open-3");
+const openButton4 = document.querySelector(".modal-open-4");
+const openButton5 = document.querySelector(".modal-open-5");
+const openButton6 = document.querySelector(".modal-open-6");
+const openButton7 = document.querySelector(".modal-open-7");
+const openButton8 = document.querySelector(".modal-open-8");
+const openButton9 = document.querySelector(".modal-open-9");
+const openButton10 = document.querySelector(".modal-open-10");
+const uploadButton = document.querySelector("#uploadBtn");
+const uploads = document.querySelector("#uploads");
+const infoButton = document.querySelector("#info");
+const prevButton = document.querySelector("#previewBtn");
+const preview = document.querySelector("#preview");
 
 closeButton.addEventListener("click", function () {
   modal.classList.toggle("closed");
-  //   modalOverlay.classList.toggle("closed");
 });
 
-openButton.addEventListener("click", function () {
+openButton1.addEventListener("click", function () {
   modal.classList.toggle("closed");
-  //   modalOverlay.classList.toggle("closed");
+});
+openButton2.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+});
+openButton3.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+});
+openButton4.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+});
+openButton5.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+});
+openButton6.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+});
+openButton7.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+});
+openButton8.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+});
+openButton9.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+});
+openButton10.addEventListener("click", function () {
+  modal.classList.toggle("closed");
 });
 
-uploadsButton.addEventListener("click", function () {
-  uploads.classList.toggle("closed");
+uploadButton.addEventListener("click", function () {
+  uploads.classList.remove("hidden");
+  preview.classList.add("hidden");
+  information.classList.add("hidden");
+});
+
+infoButton.addEventListener("click", function () {
+  uploads.classList.add("hidden");
+  preview.classList.add("hidden");
+  information.classList.remove("hidden");
+});
+
+prevButton.addEventListener("click", function () {
+  uploads.classList.add("hidden");
+  information.classList.add("hidden");
+  preview.classList.remove("hidden");
 });
